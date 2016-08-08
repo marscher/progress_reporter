d8d82c457c8177dc41ed37c9b4cd58f5ceb1f534
Martin K. Scherer <m.scherer@fu-berlin.de>
2016-08-08 16:31:47 +0200

    docstring

--- examples/simple.py
+++ examples/simple.py
@@ -5,9 +5,12 @@ import time
 class ExampleWorker(ProgressReporter):
     def __init__(self, n_jobs=100):
         self.n_jobs = n_jobs
+        """ register the amount of work with the given description """
         self._progress_register(n_jobs, description='Dispatching jobs')
 
     def work(self):
+        """ do some fake work (sleep) and update the progress via the reporter
+        """
         for job in (lambda: time.sleep(0.1) for _ in range(self.n_jobs)):
             job()
             # indicate we've finished one job, to update the progress bar
