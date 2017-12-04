875ab3c8900a4cb4b4686213f554686c3be67dcb
marscher <m.scherer@fu-berlin.de>
2017-12-04 19:49:34 +0100

    updated

--- progress_reporter/__init__.py
+++ progress_reporter/__init__.py
@@ -1,5 +1,78 @@
+"""
+Examples
+--------
 
-from .reporter import ProgressReporter
+Image you have a class doing some heavy calculations, which are split into several
+jobs/tasks/threads etc.
+
+In order to visualize the progress, one just needs to derive the worker class from
+progress_reporter.ProgressReporter and invoke the **_progress_register** method
+to tell the reporter how many pieces of work have to be done. Then the reporter
+is instructed by **_progress_update(n)** how many of pieces of work have been
+dispatched.
+
+Note that these are "private" to use this class as a mixin class and not polute the
+public interface.
+
+.. code:: python
+
+    from progress_reporter import ProgressReporter
+    import time
+
+    class ExampleWorker(ProgressReporter):
+        def __init__(self, n_jobs=100):
+            self.n_jobs = n_jobs
+            # register the amount of work with the given description
+            self._progress_register(n_jobs, description='Dispatching jobs')
+
+        def work(self):
+            # do some fake work (sleep) and update the progress via the reporter
+            for job in (lambda: time.sleep(0.1) for _ in range(self.n_jobs)):
+                job()
+                # indicate we've finished one job, to update the progress bar
+                self._progress_update(1)
+
+
+It also supports multi-stage sequential work loads by setting the parameter **stage**.
+This is just the dictionary key to the underlying process:
+
+.. code:: python
+
+    class MultiStageWorker(ProgressReporter):
+        def __init__(self, n_jobs_init, n_jobs):
+            self.n_jobs_init = n_jobs_init
+            self.n_jobs = n_jobs
+            # register an expensive initialization routine
+            self._progress_register(self.n_jobs_init, description='initializing', stage=0)
+            # register the main computation
+            self._progress_register(self.n_jobs, description='main computation', stage=1)
+
+        def work(self):
+            # do the initialization
+            for job in (lambda: time.sleep(0.1) for _ in range(self.n_jobs_init)):
+                job()
+                self._progress_update(1, stage=0)
+
+            # perform the next stage of the algorithm
+            for job in (lambda: time.sleep(0.2) for _ in range(self.n_jobs)):
+                job()
+                self._progress_update(1, stage=1)
+
+Since version 2.0 there is also a version of the this class suitable for compositions. Note that
+
+.. code:: python
+    from progress_reporter import ProgressReporter_
+
+    class Estimator(object):
+        def fit(self, X, y=None):
+            pg = ProgressReporter_()
+            pg.register(100, description='work')
+            with pg.context(): # ensure progress bars are closed if an exception occurs.
+                pg.update(50)
+                # ...
+
+"""
+from .reporter import ProgressReporter, ProgressReporter_
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- progress_reporter/reporter.py
+++ progress_reporter/reporter.py
@@ -1,16 +1,35 @@
-
-'''
-Created on 16.07.2015
-
-@author: marscher
-'''
-
-from __future__ import absolute_import, print_function
-
-from .bar import ProgressBar as _ProgressBar
-from .bar import show_progressbar as _show_progressbar
-from .bar.gui import hide_progressbar as _hide_progressbar
 from numbers import Integral
+
+
+def _simple_memorize(f):
+    # cache function f result (takes no arguments)
+    from functools import wraps
+    @wraps(f)
+    def wrapper():
+        if not hasattr(f, 'res'):
+            f.res = f()
+        return f.res
+    return wrapper
+
+
+@_simple_memorize
+def _attached_to_ipy_notebook_with_widgets():
+    try:
+        # check for widgets
+        import ipywidgets
+        if ipywidgets.version_info[0] < 4:
+            raise ImportError()
+        # check for ipython kernel
+        from IPython import get_ipython
+        ip = get_ipython()
+        if ip is None:
+            return False
+        if not getattr(ip, 'kernel', None):
+            return False
+        # No further checks are feasible
+        return True
+    except ImportError:
+        return False
 
 
 class ProgressReporter(object):
@@ -54,7 +73,40 @@ class ProgressReporter(object):
             self.__prog_rep_callbacks = {}
         return self.__prog_rep_callbacks
 
-    def _progress_register(self, amount_of_work, description='', stage=0):
+    def _progress_context(self, stage='all'):
+        """
+
+        Parameters
+        ----------
+        stage: str, iterable of keys, dict_key
+
+        Returns
+        -------
+        context manager
+        """
+        from contextlib import contextmanager
+        @contextmanager
+        def ctx():
+            try:
+                yield
+            finally:
+                if stage == 'all':
+                    keys = tuple(self._prog_rep_progressbars.keys())
+                    for s in keys:
+                        self._progress_force_finish(stage=s)
+                elif isinstance(stage, (tuple, list)):
+                    for s in stage:
+                        self._progress_force_finish(s)
+                else:
+                    self._progress_force_finish(stage)
+        return ctx()
+
+    def __check_stage_registered(self, stage):
+        if stage not in self._prog_rep_progressbars:
+            raise RuntimeError('call _progress_register(amount_of_work, stage={}) on this instance first!'.format(stage))
+        return self._prog_rep_progressbars[stage]
+
+    def _progress_register(self, amount_of_work, description='', stage=0, tqdm_args=None):
         """ Registers a progress which can be reported/displayed via a progress bar.
 
         Parameters
@@ -71,46 +123,53 @@ class ProgressReporter(object):
         if not self.show_progress:
             return
 
-        if not isinstance(amount_of_work, Integral):
-            raise ValueError("amount_of_work has to be of integer type. But is %s"
-                             % type(amount_of_work))
+        if tqdm_args is None:
+            tqdm_args = {}
 
-        # if we do not have enough work to do for the overhead of a progress bar,
-        # we just define a dummy here
+        if not isinstance(amount_of_work, Integral):
+            raise ValueError('amount_of_work has to be of integer type. But is {}'.format(type(amount_of_work)))
+
+        # if we do not have enough work to do for the overhead of a progress bar just dont create a bar.
         if amount_of_work <= ProgressReporter._pg_threshold:
-            class dummy(object):
-                pass
-            pg = dummy()
-            pg.__str__ = lambda: description
-            pg.__repr__ = pg.__str__
-            pg._dummy = None
-            pg.description = ''
+            pg = None
         else:
-            pg = _ProgressBar(amount_of_work, description=description)
+            import sys
+            # progress_reporter < 2.0 print to stdout.
+            if 'file' not in tqdm_args:
+                tqdm_args['file'] = sys.stdout
+            if 'dynamic_ncols' not in tqdm_args:
+                tqdm_args['dynamic_ncols'] = True
+            if 'description' in tqdm_args:
+                tqdm_args.pop('description')
+            args = dict(total=amount_of_work, desc=description, **tqdm_args)
+            if _attached_to_ipy_notebook_with_widgets():
+                from .notebook import my_tqdm_notebook
+                pg = my_tqdm_notebook(leave=False, **args)
+            else:
+                import tqdm
+                pg = tqdm.tqdm(leave=True, **args)
 
         self._prog_rep_progressbars[stage] = pg
         self._prog_rep_descriptions[stage] = description
+        assert stage in self._prog_rep_progressbars
+        assert stage in self._prog_rep_descriptions
 
     def _progress_set_description(self, stage, description):
         """ set description of an already existing progress """
-        assert hasattr(self, '_prog_rep_progressbars')
-        assert stage in self._prog_rep_progressbars
+        pg = self.__check_stage_registered(stage)
         self._prog_rep_descriptions[stage] = description
-        self._prog_rep_progressbars[stage].description = description
-        assert self._prog_rep_progressbars[stage].description == description
+        if pg:
+            pg.set_description(description, refresh=True)
 
     def register_progress_callback(self, call_back, stage=0):
         """ Registers the progress reporter.
-
         Parameters
         ----------
         call_back : function
             This function will be called with the following arguments:
-
             1. stage (int)
             2. instance of pyemma.utils.progressbar.ProgressBar
             3. optional \*args and named keywords (\*\*kw), for future changes
-
         stage: int, optional, default=0
             The stage you want the given call back function to be fired.
         """
@@ -138,57 +197,86 @@ class ProgressReporter(object):
         if not self.show_progress:
             return
 
-        if stage not in self._prog_rep_progressbars:
-            raise RuntimeError(
-                "call _progress_register(amount_of_work, stage=x) on this instance first!")
-
-        if hasattr(self._prog_rep_progressbars[stage], '_dummy'):
+        pg = self.__check_stage_registered(stage)
+        if not pg:
             return
 
-        pg = self._prog_rep_progressbars[stage]
-        pg.numerator += numerator_increment
-        # we are done
-        if pg.numerator == pg.denominator:
-            if stage in self._prog_rep_callbacks:
-                for callback in self._prog_rep_callbacks[stage]:
-                    callback(stage, pg, **kw)
-            self._progress_force_finish(stage)
-            return
-        elif pg.numerator > pg.denominator:
+        diff = pg.total - numerator_increment
+        if diff < 0:
             import warnings
             warnings.warn("This should not happen. An caller pretended to have "
                           "achieved more work than registered")
-            return
-
-        desc = self._prog_rep_descriptions[stage].format(**kw)
-        pg.description = desc
-
-        _show_progressbar(pg, show_eta=show_eta, description=desc)
-
-        if stage in self._prog_rep_callbacks:
-            for callback in self._prog_rep_callbacks[stage]:
-                callback(stage, pg, **kw)
+        else:
+            pg.update(numerator_increment)
+            if stage in self._prog_rep_callbacks:
+                for callback in self._prog_rep_callbacks[stage]:
+                    callback(stage, pg, **kw)
 
     def _progress_force_finish(self, stage=0, description=None):
         """ forcefully finish the progress for given stage """
         if not self.show_progress:
             return
-        if stage not in self._prog_rep_progressbars:
-            raise RuntimeError(
-                "call _progress_register(amount_of_work, stage=x) on this instance first!")
-        pg = self._prog_rep_progressbars[stage]
-        if not isinstance(pg, _ProgressBar):
+
+        pg = self.__check_stage_registered(stage)
+
+        if not pg:
             return
 
-        if pg.numerator < pg.denominator:
-            pg.numerator = pg.denominator
+        pg.desc = description
+        diff = pg.total - pg.n
+        if diff > 0:
+            pg.update(diff)
+        pg.refresh(nolock=True)
+        pg.close()
+        self._prog_rep_progressbars.pop(stage, None)
+        self._prog_rep_descriptions.pop(stage, None)
+        self._prog_rep_callbacks.pop(stage, None)
 
-        pg._eta.eta_epoch = 0
+    def _progress_refresh(self, stage=0):
+        if not self.show_progress:
+            return
 
-        if description is not None:
-            pg.description = description
-        else:
-            description = self._prog_rep_descriptions[stage]
+        pg = self.__check_stage_registered(stage)
+        if not pg:
+            return
+        pg.refresh()
 
-        _show_progressbar(pg, description=description)
-        _hide_progressbar(pg)
+    @property
+    def _progress_num_registered(self):
+        return len(self._prog_rep_progressbars)
+
+    @property
+    def _progress_registered_stages(self):
+        return tuple(self._prog_rep_progressbars.keys())
+
+
+class ProgressReporter_(ProgressReporter):
+    """ class to report progress for multiple stages. Suitable for composition.
+    """
+
+    def context(self, stage='all'):
+        return self._progress_context(stage=stage)
+
+    def register(self, amount_of_work, description='', stage=0, tqdm_args=None):
+        self._progress_register(amount_of_work=amount_of_work, description=description, stage=stage, tqdm_args=tqdm_args)
+
+    def update(self, increment, stage=0):
+        self._progress_update(increment, stage=stage)
+
+    def set_description(self, description, stage=0):
+        self._progress_set_description(description=description, stage=stage)
+
+    def finish(self, description=None, stage=0):
+        self._progress_force_finish(description=description, stage=stage)
+
+    def refresh(self, stage=0):
+        """ this does not need to be called manually, but can be useful in certain situations. """
+        self._progress_refresh(stage)
+
+    @property
+    def num_registered(self):
+        return self._progress_num_registered
+
+    @property
+    def registered_stages(self):
+        return self._progress_registered_stages
